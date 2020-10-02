1fe9f1e30f42416d1c97336a2ac400ccce303e3e
Nikoloz Tchavtchanidze <45031920+nchavchanidze@users.noreply.github.com>
2020-10-02 23:02:35 +0400

    Add Header, Home, Testimonial, Apartments, Mortgage Components && Fonts && Styles

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('.home-slider').slick({
+        dots: false,
+        infinite: true,
+        speed: 400,
+        autoplay: false,
+        arrows: false
+    });
+  });
